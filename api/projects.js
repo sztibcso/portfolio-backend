62b488ff7f53fbc2717583a176fd7673ee077145
sztibcso <szalontai.tib@gmail.com>
2025-04-29 23:07:48 +0200

    image change

--- api/projects.js
+++ api/projects.js
@@ -4,17 +4,17 @@ export default function handler(req, res) {
     const projects = [
       {
         id: 1,
-        frontImage: "https://via.placeholder.com/300x450",
+        frontImage: "https://picsum.photos/300/450",
         description: "Ez az első projekt backendből!",
-        linkImage: "https://via.placeholder.com/50",
-        linkUrl: "https://example.com/project1",
+        linkImage: "https://picsum.photos/300/450",
+        linkUrl: "https://picsum.photos/300/450",
       },
       {
         id: 2,
-        frontImage: "https://via.placeholder.com/300x450",
+        frontImage: "https://picsum.photos/300/450",
         description: "Második projekt backendből!",
-        linkImage: "https://via.placeholder.com/50",
-        linkUrl: "https://example.com/project2",
+        linkImage: "https://picsum.photos/300/450",
+        linkUrl: "https://picsum.photos/300/450",
       },
     ];
   
