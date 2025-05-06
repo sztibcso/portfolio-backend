b4449f2c7a9f8611c8e6f08bf43826867f59482a
sztibcso <szalontai.tib@gmail.com>
2025-05-06 12:22:01 +0200

    Projects changes, react native changed to next.js

--- api/projects.js
+++ api/projects.js
@@ -5,28 +5,28 @@ export default function handler(req, res) {
     {
       id: 1,
       frontImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/angular.png", // Angular
-      description: "A pixel-style Angular project!",
+      description: "An Angular project descripton",
       linkImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/coming-soon.png", // Coming Soon
       linkUrl: "/coming-soon",
     },
     {
       id: 2,
-      frontImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/react-native.png", // React Native
-      description: "A pixel-style React Native project!",
+      frontImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/nextjs.png", // Next.js
+      description: "A Next.js project description!",
       linkImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/coming-soon.png", // Coming Soon
       linkUrl: "/coming-soon",
     },
     {
       id: 3,
       frontImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/vue.png", // Vue
-      description: "A pixel-style Vue project!",
+      description: "A Vue project descripton!",
       linkImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/coming-soon.png", // Coming Soon
       linkUrl: "/coming-soon",
     },
     {
       id: 4,
       frontImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/web3.png", // web3
-      description: "A pixel-style web3 project!",
+      description: "A Web3 project descripton!",
       linkImage: "https://portfolio-szalontai-tibors-projects.vercel.app/images/coming-soon.png", // Coming Soon
       linkUrl: "/coming-soon",
     },
